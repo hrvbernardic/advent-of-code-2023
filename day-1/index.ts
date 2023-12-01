5fc6c1ceba0dbff40506ca3fc696760661da510c
Dodo <hrv.bernardic@gmail.com>
2023-12-01 19:03:41 +0100

    day 1 - part 1

--- day-1/index.ts
+++ day-1/index.ts
@@ -1,1 +1,29 @@
-console.log('day1');
+import {createInterface} from "readline";
+import {join} from "path";
+import {createReadStream} from "fs";
+
+const fileStream = createReadStream(join(__dirname, 'input.txt'));
+
+const rl = createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+let calibrationValues = [];
+
+rl.on('line', (line: string) => {
+    calibrationValues.push(getCalibrationValue(line));
+});
+
+rl.on('close', () => {
+    const sum = calibrationValues.reduce((sum, value) => sum + value, 0);
+    console.log(sum);
+});
+
+function getCalibrationValue(text: string) {
+    const numbers = text.replace(/[^0-9]/g, '');
+    return parseInt(`${numbers.at(0)}${numbers.at(-1)}`, 10);
+}
+
+
+
