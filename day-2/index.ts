36d4a3e97be1eb21528ef99e31cbee2b1d6f96bb
Dodo <hrv.bernardic@gmail.com>
2023-12-03 00:50:58 +0100

    day 2

---
+++ day-2/index.ts
@@ -0,0 +1,109 @@
+import {createReadStream} from "fs";
+import {join} from "path";
+import {createInterface} from "readline";
+
+type CubeTypes = 'blue' | 'red' | 'green';
+type CubeCounts = {[color in CubeTypes]: number};
+
+const cubeLimits: {[key in CubeTypes]: number} = {
+    red: 12,
+    green: 13,
+    blue: 14
+}
+
+const fileStream = createReadStream(join(__dirname, 'input.txt'));
+
+const rl = createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+let games = [];
+
+rl.on('line', (line: string) => {
+    games.push(line)
+});
+
+rl.on('close', () => {
+    const indexSum = getValidGameIndexesSum(games);
+    const powerSum = getSumOfPowerOfSets(games);
+    console.log(indexSum, powerSum);
+});
+
+function getSumOfPowerOfSets(games: string[]) {
+    let powerSum = 0;
+    games.forEach(game => {
+        const [gameInfo, cubes] = game.split(':');
+        const rounds = cubes.split(';').map(c => c.trim());
+        const minCubeCount: CubeCounts = {
+            blue: 0,
+            red: 0,
+            green: 0
+        };
+
+        rounds
+            .map(getCubeCounts)
+            .forEach(counts => {
+                Object
+                    .entries(counts)
+                    .forEach(([color, count]) => {
+                        if (minCubeCount[color] < count) minCubeCount[color] = count;
+                    })
+            });
+
+        powerSum += minCubeCount.green * minCubeCount.red * minCubeCount.blue;
+    });
+
+    return powerSum;
+
+}
+
+function getValidGameIndexesSum(games: string[]) {
+    let validGamesIndexSum = 0;
+    games.forEach(game => {
+        const [gameInfo, cubes] = game.split(':');
+        const gameIndex = parseInt(gameInfo.split(' ').at(-1));
+        const rounds = cubes.split(';').map(c => c.trim());
+
+        const isGameValid = rounds
+            .map(r => {
+                const cubeCounts = getCubeCounts(r);
+                return checkValidCubeCounts(cubeCounts);
+            })
+            .reduce((acc, curr) => acc && curr, true);
+
+
+        if (isGameValid) {
+            validGamesIndexSum += gameIndex;
+        }
+
+    });
+
+    return validGamesIndexSum;
+}
+
+function getCubeCounts(round: string): CubeCounts{
+    const colors = round.split(',').map(r => r.trim());
+
+    const colorCounts = {
+        blue: 0,
+        red: 0,
+        green: 0
+    };
+
+    colors.forEach(c => {
+        const [stringValue, color] = c.split(' ');
+        colorCounts[color] = parseInt(stringValue);
+    });
+
+    return colorCounts;
+
+}
+
+function checkValidCubeCounts(counts: CubeCounts) {
+    if (counts.red > cubeLimits.red) return false;
+    if (counts.blue > cubeLimits.blue) return false;
+    if (counts.green > cubeLimits.green) return false;
+    return true;
+
+}
