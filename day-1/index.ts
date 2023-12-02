17204d89b91086285415a42eb7b144079f09c73f
Dodo <hrv.bernardic@gmail.com>
2023-12-02 23:38:04 +0100

    day 1 - part 1

--- day-1/index.ts
+++ day-1/index.ts
@@ -12,18 +12,57 @@ const rl = createInterface({
 let calibrationValues = [];
 
 rl.on('line', (line: string) => {
-    calibrationValues.push(getCalibrationValue(line));
+    calibrationValues.push(getStringCalibrationValue(line));
 });
 
 rl.on('close', () => {
-    const sum = calibrationValues.reduce((sum, value) => sum + value, 0);
-    console.log(sum);
+    const stringSum = calibrationValues.reduce((sum, value) => sum + value, 0);
+    console.log(stringSum);
 });
 
-function getCalibrationValue(text: string) {
-    const numbers = text.replace(/[^0-9]/g, '');
-    return parseInt(`${numbers.at(0)}${numbers.at(-1)}`, 10);
+// String
+const validStringMap = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
 }
 
+function getStringCalibrationValue(text: string) {
+    const occurrences: any = [];
 
+    const matches = Object.keys(validStringMap)
+        .flatMap((str) => getAllOccurrences(text, str).map(o => ({index: o.index, val: validStringMap[o.value]})))
+        .sort((a,b) => a.index - b.index);
 
+    const first = matches.at(0).val;
+    const last = matches.at(-1).val;
+
+    return parseInt(`${first}${last}`);
+}
+
+function getAllOccurrences(mainString: string, subString: string) {
+    let indices = [];
+    let currentIndex = mainString.indexOf(subString);
+
+    while (currentIndex !== -1) {
+        indices.push(currentIndex);
+        currentIndex = mainString.indexOf(subString, currentIndex + 1);
+    }
+
+    return indices.map(i => ({index: i, value: subString}));
+}
